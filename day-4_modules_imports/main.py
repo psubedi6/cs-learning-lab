3119a8c3bc534da8154b322ec04e242904ab1bf0
Prakash Subedi <prakassubedi6@gmail.com>
2026-07-04 23:02:32 -0400

    feat: custom module and import demo

---
+++ day-4_modules_imports/main.py
@@ -0,0 +1,27 @@
+"""import math
+print(math.sqrt(25))
+print(math.pi)
+
+from math import sqrt
+print(sqrt(25))
+
+import math as m
+print(m.sqrt(25))
+print(m.pi)
+
+from math import sqrt as s
+print(s(25))
+
+import my_tools as mt
+print(mt.student_name)
+print(mt.course)
+print(mt.day_number)
+
+from my_tools import student_name, course
+print(student_name)
+print(course)"""
+
+import my_tools
+
+print("main.py is running")
+print(my_tools.student_name)
---
+++ day-4_modules_imports/my_tools.py
@@ -0,0 +1,6 @@
+student_name = "Prakash"
+course = "Python Core"
+day_number = 4
+
+if __name__ == "__main__":
+    print("my_tools.py is running")
